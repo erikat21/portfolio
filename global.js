9c9f4f86ec911bed34f82f4d612ca9f0b215c2b5
erikat21 <ertong@ucsd.edu>
2025-10-12 12:46:22 -0700

    step 3 done

--- global.js
+++ global.js
@@ -4,10 +4,46 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-const navLinks = $$("nav a");
+// const navLinks = $$("nav a");
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname,
-);
+// let currentLink = navLinks.find(
+//   (a) => a.host === location.host && a.pathname === location.pathname,
+// );
 
-currentLink?.classList.add('current');
+// currentLink?.classList.add('current');
+
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+  ? "/"                  // Local server
+  : "/portfolio/";         // GitHub Pages repo name
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: "https://github.com/erikat21", title: 'GitHub' },
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+for (let p of pages) {
+    let url = !p.url.startsWith('http') ? BASE_PATH + p.url : p.url;
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    // if (a.host === location.host && a.pathname === location.pathname) {
+    //     a.classList.add('current');
+    // }
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname,
+    );
+    if (new URL(a.href).host !== location.host) {
+        a.target = "_blank";
+        a.rel = "noopener noreferrer";
+    }
+
+    nav.append(a);  
+}
+
