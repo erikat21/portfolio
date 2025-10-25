0f202c6d074e9112d8fed7f0ffb106eb3b872b2d
erikat21 <ertong@ucsd.edu>
2025-10-25 01:25:01 -0700

    step 1 done

--- projects/projects.js
+++ projects/projects.js
@@ -8,3 +8,26 @@ import { fetchJSON, renderProjects } from '../global.js';
         titleElement.textContent = `${projects.length} Projects`;
     }
 })();
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2, 3, 4, 5, 5];
+let total = 0;
+
+for (let d of data) {
+  total += d;
+}
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let arcs = arcData.map((d) => arcGenerator(d));
+arcs.forEach((arc, idx) => {
+  d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx));
+});
