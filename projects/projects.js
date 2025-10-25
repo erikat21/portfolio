9faf5f5a793c44032d28cec8b866256fcae174d3
erikat21 <ertong@ucsd.edu>
2025-10-25 11:43:07 -0700

    step 2 done

--- projects/projects.js
+++ projects/projects.js
@@ -10,24 +10,33 @@ import { fetchJSON, renderProjects } from '../global.js';
 })();
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+let sliceGenerator = d3.pie().value((d) => d.value);
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
-
-let data = [1, 2, 3, 4, 5, 5];
-let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
-arcs.forEach((arc, idx) => {
-  d3.select('svg').append('path').attr('d', arc).attr('fill', colors(idx));
+d3.select('svg')
+        .selectAll('path')
+        .data(arcData)
+        .join('path')
+        .attr('d', d => arcGenerator(d))
+        .attr('fill', (d, i) => colors(i))
+        .attr('transform', 'translate(50, 50)');
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend
+    .append('li')
+    .attr('class', 'legend-item')
+    .attr('style', `--color:${colors(idx)}`) // set the style attribute while passing in parameters
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`); // set the inner html of <li>
 });
