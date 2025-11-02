9e12c725b95476d6cb8d8c325c6b1a68ca2d3afa
erikat21 <ertong@ucsd.edu>
2025-11-02 15:35:35 -0800

    step 0 done

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 import { fetchJSON, renderProjects, fetchGitHubData } from './global.js';
 (async function loadLatestProjects() {
-    try {
-        const projects = await fetchJSON('./lib/projects.json');
-        const latestProjects = projects.slice(0, 3);
-        const projectsContainer = document.querySelector('.projects');
-        renderProjects(latestProjects, projectsContainer, 'h3');
-    } catch (err) {
-    console.error('Error loading latest projects:', err);
-    }
+  try {
+    const projects = await fetchJSON('./lib/projects.json');
+    const latestProjects = projects.slice(0, 3);
+    const projectsContainer = document.querySelector('.projects');
+    renderProjects(latestProjects, projectsContainer, 'h3');
+  } catch (err) {
+  console.error('Error loading latest projects:', err);
+  }
 })();
 
 (async function loadHomepage() {
-    try {
-        const githubData = await fetchGitHubData('erikat21');
-        console.log('GitHub data loaded:', githubData);
-        const profileStats = document.querySelector('#profile-stats');
-        if (profileStats) {
-            profileStats.innerHTML = `
-            <dl>
-                <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-                <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-                <dt>Followers:</dt><dd>${githubData.followers}</dd>
-                <dt>Following:</dt><dd>${githubData.following}</dd>
-            </dl>
+  try {
+    const githubData = await fetchGitHubData('erikat21');
+    console.log('GitHub data loaded:', githubData);
+    const profileStats = document.querySelector('#profile-stats');
+    if (profileStats) {
+      profileStats.innerHTML = `
+        <dl>
+          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+          <dt>Followers:</dt><dd>${githubData.followers}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
+        </dl>
         `;
         }
     }  catch (err) {
