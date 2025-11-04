593cbbb601f64660785842c54b6a2fc92603bddc
erikat21 <ertong@ucsd.edu>
2025-11-03 18:04:31 -0800

    step 4 done

--- meta/main.js
+++ meta/main.js
@@ -143,20 +143,29 @@ const gridlines = svg
 gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
 
 const dots = svg.append('g').attr('class', 'dots');
+const [minLines, maxLines] = d3.extent(commits, (d) => d.totalLines);
+const sortedCommits = d3.sort(commits, (d) => -d.totalLines);
+const rScale = d3
+  .scaleSqrt() // Change only this line
+  .domain([minLines, maxLines])
+  .range([2, 30]);
 dots
   .selectAll('circle')
-  .data(commits)
+  .data(sortedCommits)
   .join('circle')
   .attr('cx', (d) => xScale(d.datetime))
   .attr('cy', (d) => yScale(d.hourFrac))
-  .attr('r', 5)
   .attr('fill', 'var(--color-accent)')
+  .attr('r', (d) => rScale(d.totalLines))
+  .style('fill-opacity', 0.7) // Add transparency for overlapping dots
   .on('mouseenter', (event, commit) => {
+    d3.select(event.currentTarget).style('fill-opacity', 1); // Full opacity on hover
     renderTooltipContent(commit);
     updateTooltipVisibility(true);
     updateTooltipPosition(event);
   })
-  .on('mouseleave', () => {
+  .on('mouseleave', (event) => {
+    d3.select(event.currentTarget).style('fill-opacity', 0.7);
     updateTooltipVisibility(false);
   });
 
