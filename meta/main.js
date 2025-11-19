bd67b0f47e1f02e8632cf0bbef56a4e3386ba5cf
erikat21 <ertong@ucsd.edu>
2025-11-18 16:29:12 -0800

    step 2 done

--- meta/main.js
+++ meta/main.js
@@ -164,7 +164,8 @@ function renderScatterPlot(data, commits) {
   // Brush
   const brush = d3.brush()
     .extent([[margin.left, margin.top], [margin.left + usableWidth, margin.top + usableHeight]])
-    .on('start brush end', (event) => brushed(event, commits));
+    .on('start brush end', (event) => brushed(event, filteredCommits));
+
 
   svg.call(brush);
 
@@ -231,6 +232,9 @@ function updateScatterPlot(data, commits) {
       d3.select(event.currentTarget).style('fill-opacity', 0.7);
       updateTooltipVisibility(false);
     });
+  // svg.select('.brush')
+  //   .call(d3.brush().extent([[margin.left, margin.top], [margin.left + usableWidth, margin.top + usableHeight]])
+  //   .on('start brush end', (event) => brushed(event, filteredCommits)));
 }
 
 function renderTooltipContent(commit) {
@@ -356,7 +360,48 @@ function updateSliderDisplay() {
   timeEl.textContent = commitMaxTime.toLocaleString();
   filteredCommits = commits.filter((d) => d.datetime <= commitMaxTime);
   updateScatterPlot(data, filteredCommits);
+  updateFileDisplay(filteredCommits)
 }
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+function updateFileDisplay(filteredCommits){
+  let lines = filteredCommits.flatMap((d) => d.lines);
+  let files = d3
+  .groups(lines, (d) => d.file)
+  .map(([name, lines]) => {
+    return { name, lines };
+  })
+  .sort((a, b) => b.lines.length - a.lines.length);
+  let filesContainer = d3
+  .select('#files')
+  .selectAll('div')
+  .data(files, (d) => d.name)
+  .join(
+    // This code only runs when the div is initially rendered
+    (enter) =>
+      enter.append('div').call((div) => {
+        div.append('dt').append('code');
+        div.select('dt').append('small')
+        div.append('dd');
+      }),
+  );
+
+  // This code updates the div info
+  filesContainer.select('dt > code').text((d) => d.name);
+  filesContainer.select('dt > small')
+    .text(d => `${d.lines.length} lines`)
+    .style('display', 'block')
+    .style('opacity', 0.6)
+    .style('font-size', '0.8em');
+  filesContainer.select('dd')
+    .selectAll('div.loc')
+    .data(d => d.lines)
+    .join('div')
+    .attr('class', 'loc')
+    .style('background-color', d => colors(d.type));
+}
+
 const timeSlider = document.getElementById("commit-progress");
 
 timeSlider.addEventListener('input', updateSliderDisplay)
